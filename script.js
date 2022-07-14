03439681a100c9143d6dff0529e4a7d8105e810a
Noah <nleyja@gmail.com>
2022-07-14 18:30:30 -0500

    added three number return function

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ console.log(positiveOrNegativeNumber(2));
 console.log(positiveOrNegativeNumber(-3));
 console.log(positiveOrNegativeNumber(0));
 console.log(positiveOrNegativeNumber(389));
+
+// 2. Write a function that takes a number of days and converts it into an age.
+
+
+
+
+// 3. Write a function that takes three numbers and returns the largest of the three numbers.
+function getLargestNumber(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+
+}
+console.log(getLargestNumber(2, 1, 4));
